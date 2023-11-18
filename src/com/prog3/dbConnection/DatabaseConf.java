81058aec0996f01e5fcc124feab22296f0c421b3
Lovatiana15 <hei.lovatiana@gmail.com>
2023-11-18 15:15:48 +0200

    feat: Adds a database configuration class

--- src/com/prog3/dbConnection/DatabaseConf.java
+++ src/com/prog3/dbConnection/DatabaseConf.java
@@ -1,5 +1,30 @@
-package com.prog3.dbConnection;
+package com.prog3.dbConnection; 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseConf {
-    
+    private Connection connection;
+
+    public DatabaseConf() {
+        String jdbcURL = System.getenv("JDBC_URL");
+        System.out.println("aaa0: " + jdbcURL);
+        String username = System.getenv("DB_USERNAME");
+        String password = System.getenv("DB_PASSWORD");
+
+        try {
+            connection = DriverManager.getConnection(jdbcURL, username, password);
+            if (connection != null) {
+                System.out.println("Database connection established!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Unable to connect to database.", e);
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
