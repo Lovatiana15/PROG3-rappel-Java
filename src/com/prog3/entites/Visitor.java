672526a40facf0f37332d1e0df6d0b8b2be43075
Lovatiana15 <hei.lovatiana@gmail.com>
2023-11-18 15:30:59 +0200

    feat: Adds Visitor entity class for managing Visitor

--- src/com/prog3/entites/Visitor.java
+++ src/com/prog3/entites/Visitor.java
@@ -1,5 +1,28 @@
 package com.prog3.entites;
 
-public class Visitor {
-    
+public class Visitor extends User {
+    private String matriculeNumber; 
+
+    // Constructeur 
+    public Visitor(int userId, String username, String password, String matriculeNumber) {
+        super(userId, username, password);
+        this.matriculeNumber = matriculeNumber; 
+    }
+
+    // Getter et setter 
+    public String getMatriculeNumber() {
+        return matriculeNumber;
+    }
+
+    public void setMatriculeNumber(String matriculeNumber) {
+        this.matriculeNumber = matriculeNumber;
+    }
+    @Override
+    public String toString() {
+        return "Visitor{" +
+                "username='" + getUsername() + '\'' +
+                ", password='" + getPassword() + '\'' +
+                ", matriculeNumber='" + matriculeNumber + '\'' +
+                '}';
+    }
 }
