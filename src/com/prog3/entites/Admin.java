41b3edf2c405244cdbfce338461054decc3cb6fc
Lovatiana15 <hei.lovatiana@gmail.com>
2023-11-18 15:20:01 +0200

    feat: Adds Admin entity class for managing administrators

--- src/com/prog3/entites/Admin.java
+++ src/com/prog3/entites/Admin.java
@@ -1,5 +1,28 @@
 package com.prog3.entites;
 
-public class Admin {
-    
+public class Admin extends User {
+    private String adminRole; 
+
+    // Constructeur 
+    public Admin(int userId, String username, String password, String adminRole) {
+        super(userId, username, password); 
+        this.adminRole = adminRole; 
+    }
+
+    // Getter et setter 
+    public String getAdminRole() {
+        return adminRole;
+    }
+
+    public void setAdminRole(String adminRole) {
+        this.adminRole = adminRole;
+    }
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "username='" + getUsername() + '\'' +
+                ", password='" + getPassword() + '\'' +
+                ", adminRole='" + adminRole + '\'' +
+                '}';
+    }
 }
